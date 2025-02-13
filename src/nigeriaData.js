834d2845ee30e47caf47cb81011971339058b85d
Oluwakorede Akintayo <akintayo.oluwakorede@gmail.com>
2025-02-13 03:47:01 +0100

    Updated components

--- src/nigeriaData.js
+++ src/nigeriaData.js
@@ -1,21 +1,42 @@
 const nigeriaData = {
-  "Lagos": ["Ikeja", "Ikorodu", "Epe",   "Lagos Island",  "Badagry", "Magodo","Ojo", ],
+  "Abia": ["Aba", "Umuahia", "Ohafia", "Arochukwu", "Bende",],
+  "Adamawa": ["Yola", "Mubi", "Jimeta"],
+  "Akwa Ibom": ["Uyo", "Eket", "Ikot Ekpene"],
+  "Anambra": ["Awka", "Onitsha", "Nnewi"],
+  "Bauchi": ["Bauchi", "Azare", "Misau"],
+  "Bayelsa": ["Yenagoa", "Ogbia", "Sagbama"],
+  "Benue": ["Makurdi", "Gboko", "Otukpo"],
+  "Borno": ["Maiduguri", "Biu", "Chibok"],
+  "Cross River": ["Calabar", "Ikom", "Ogoja"],
+  "Delta": ["Asaba", "Warri", "Ughelli"],
+  "Ebonyi": ["Abakaliki", "Afikpo", "Onueke"],
+  "Edo": ["Benin City", "Ekpoma", "Auchi"],
   "Ekiti": ["Ado-Ekiti", "Ikere-Ekiti", "Ijero-Ekiti"],
-
-  "Ogun": ["Abeokuta", ],
-
-  "Oyo": ["Ibadan", "Ogbomosho",  "Iseyin","Saki","Igboho","Kishi","Eruwa", "Akinyele","Alakia"],
-
-
-  "Kano": [ "Wudil", "Gwarzo", "Kumbotso",
+  "Enugu": ["Enugu", "Nsukka", "Agbani"],
+  "Gombe": ["Gombe", "Kumo", "Bajoga"],
+  "Imo": ["Owerri", "Orlu", "Ihuo"],
+  "Jigawa": ["Dutse", "Hadejia", "Gumel"],
+  "Kaduna": ["Kaduna", "Zaria", "Kafanchan"],
+  "Kano": ["Kano", "Wudil", "Dambatta",  "Gwarzo", "Kumbotso",
     "Rano","Bichi","Dala","Tarauni", "Ungogo"],
 
-
-  "Kaduna": ["Kaduna", "Zaria", "Kafanchan"],
-
-
-  "Abia":["Umuahia","Ohafia","Arochukwu", "Bende", 
-    ]
+  "Katsina": ["Katsina", "Daura", "Funtua"],
+  "Kebbi": ["Birnin Kebbi", "Argungu", "Yauri"],
+  "Kogi": ["Lokoja", "Idah", "Kabba"],
+  "Kwara": ["Ilorin", "Offa", "Jebba"],
+ "Lagos": ["Ikeja", "Ikorodu", "Epe",   "Lagos Island",  "Badagry", "Magodo","Ojo", ],
+  "Nasarawa": ["Lafia", "Keffi", "Akwanga"],
+  "Niger": ["Minna", "Suleja", "Bida"],
+  "Ogun": ["Abeokuta"],
+  "Ondo": ["Akure", "Ondo", "Owo"],
+  "Osun": ["Osogbo", "Ile-Ife", "Ilesa"],
+ "Oyo": ["Ibadan", "Ogbomosho",  "Iseyin","Saki","Igboho","Kishi","Eruwa", "Akinyele","Alakia"],
+  "Plateau": ["Jos", "Barkin Ladi", "Pankshin"],
+  "Rivers": ["Port Harcourt", "Bonny", "Omoku", "Okirika", "Opobo"],
+  "Sokoto": ["Sokoto", "Wurno", "Tambuwal"],
+  "Taraba": ["Jalingo", "Wukari", "Serti"],
+  "Yobe": ["Damaturu", "Potiskum", "Gashua"],
+  "Zamfara": ["Gusau", "Kaura Namoda", "Anka"]
 };
 
 export default nigeriaData;
